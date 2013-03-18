5a4cb7755138fd3cc95885d70cffbee620c932e6
Alex Dobson <alexander.dobson@lexisnexis.com>
2013-03-18 13:21:14 +0000

    Removed an unused variable

--- js/app/app.js
+++ js/app/app.js
@@ -1,13 +1,17 @@
-define(['backbone-min'], function() {
+define(['view/v_mainNav', 'backbone-min'], function(mainNav) {
 
 //        _.extend(Backbone.View.prototype, {
 //            // Example of how to extend the Backbone.View object
 //            getId: function() { return this.$el.selector.replace('#', '') }
 //        });
 
+
+        mainNav.init();
+
         return {
             init: function(){
                 console.info('App module initiated')
             }
         }
-});
+    }
+);
