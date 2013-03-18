1e297581cdecc76172f339a13f699e34dcf4117a
Alex Dobson <alexander.dobson@lexisnexis.com>
2013-03-18 13:15:29 +0000

    Updated app.js to contain an init function which config already calls. Also added an example of how to extend the backbone view object

--- js/app/app.js
+++ js/app/app.js
@@ -1,3 +1,13 @@
-define(['backbone-min'], function(mainNav) {
-	
+define(['backbone-min'], function() {
+
+//        _.extend(Backbone.View.prototype, {
+//            // Example of how to extend the Backbone.View object
+//            getId: function() { return this.$el.selector.replace('#', '') }
+//        });
+
+        return {
+            init: function(){
+                console.info('App module initiated')
+            }
+        }
 });
